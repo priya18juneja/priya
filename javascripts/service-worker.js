65fe698664e4f80e900b2b7be4014aab5b003298
priya18juneja <priyajuneja18@gmail.com>
2016-09-11 13:31:07 +0530

    Update service-worker.js

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,5 +1,5 @@
-/*if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/priya/javascripts/service-worker.js').then(function(registration) {
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/priya/javascripts/service-worker.js').then(function(initialiseState) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
      document.getElementById('not-status').innerHTML = '<h4>mobiForge notifications</h4><input type="checkbox" name="pushStatus" id="pushStatus" value="false" /><label for="pushStatus">Receive push notifications for new content</label><div id="pushStatusMsg"></div>';
@@ -7,20 +7,8 @@
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
   });
-}*/
+}
 
-
-
-
-  // Check that service workers are supported, if so, progressively  
-  // enhance and add push messaging support, otherwise continue without it.  
-  if ('serviceWorker' in navigator) {  
-    navigator.serviceWorker.register('/priya/javascripts/service-worker.js')  
-    .then(initialiseState);  
-  } else {  
-    console.warn('Service workers aren\'t supported in this browser.');  
-  }  
-});
 function initialiseState() {  
   alert("initialise0");
   // Are Notifications supported in the service worker?  
