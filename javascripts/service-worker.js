0442edfb502dfbb2e244048e05f1cfe67e4e143c
priya18juneja <priyajuneja18@gmail.com>
2016-09-11 09:26:04 +0530

    Update service-worker.js

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -2,6 +2,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/priya/javascripts/service-worker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+     document.getElementById('not-status').innerHTML = '<h4>mobiForge notifications</h4><input type="checkbox" name="pushStatus" id="pushStatus" value="false" /><label for="pushStatus">Receive push notifications for new content</label><div id="pushStatusMsg"></div>';
   }).catch(function(err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
