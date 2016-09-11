61dcd7637fa85be2d6c017b1364cac8832aadc56
priya18juneja <priyajuneja18@gmail.com>
2016-09-11 13:16:55 +0530

    Update service-worker.js

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -10,16 +10,7 @@
 }*/
 
 
-var isPushEnabled = false;
-window.addEventListener('load', function() {  
-  var pushButton = document.querySelector('.js-push-button');  
-  pushButton.addEventListener('click', function() {  
-    if (isPushEnabled) {  
-      unsubscribe();  
-    } else {  
-      subscribe();  
-    }  
-  });
+
 
   // Check that service workers are supported, if so, progressively  
   // enhance and add push messaging support, otherwise continue without it.  
