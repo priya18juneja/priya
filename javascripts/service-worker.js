f1c6b22694edd14ba45b8102161aa15d11f2b8e8
priya18juneja <priyajuneja18@gmail.com>
2016-09-11 13:38:02 +0530

    Update service-worker.js

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/priya/javascripts/service-worker.js').then(function(registration) {
     // Registration was successful
+    initialiseState();
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
   }).catch(function(err) {
     // registration failed :(
