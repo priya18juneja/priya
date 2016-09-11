1b9bba5bdfd2c79aaf6be77108b503cc0132f092
priya18juneja <priyajuneja18@gmail.com>
2016-09-11 13:09:08 +0530

    Update service-worker.js

--- javascripts/service-worker.js
+++ javascripts/service-worker.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/priya/javascripts/service-worker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
@@ -7,4 +7,26 @@ if ('serviceWorker' in navigator) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
   });
-}
+}*/
+
+
+var isPushEnabled = false;
+window.addEventListener('load', function() {  
+  var pushButton = document.querySelector('.js-push-button');  
+  pushButton.addEventListener('click', function() {  
+    if (isPushEnabled) {  
+      unsubscribe();  
+    } else {  
+      subscribe();  
+    }  
+  });
+
+  // Check that service workers are supported, if so, progressively  
+  // enhance and add push messaging support, otherwise continue without it.  
+  if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register('/service-worker.js')  
+    .then(initialiseState);  
+  } else {  
+    console.warn('Service workers aren\'t supported in this browser.');  
+  }  
+});
